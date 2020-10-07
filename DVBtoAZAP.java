227cfa18d7c0f5d053e694d7c88a1142319d62fa
echendri <56411897+echendri@users.noreply.github.com>
2020-10-07 14:11:39 -0700

    Add files via upload

---
+++ DVBtoAZAP.java
@@ -0,0 +1,113 @@
+package capstone;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Script for converting DVBSRC files created with w_scan to ATSC format usable
+ * with azap
+ * 
+ *  usage: convertDVBSRCtoAZAP <inputfile> <outputfile>
+ * 
+ * @author Eric
+ */
+public class DVBtoAZAP {
+
+	// usage: convertDVBSRCtoAZAP <inputfile> <outputfile>
+	public static void main(String[] args) {
+
+		File input = new File(args[0]);
+		File output = new File(args[1]);
+
+		String name;
+		String frequency;
+		String type = "8VSB";
+		String videoPID;
+		String subChannel;
+		String serviceID;
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(input));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+			String currentLine = "";
+
+			// Want to keep 1, 2 + 000, 6, last, 4 to the left of last
+			while ((currentLine = br.readLine()) != null) {
+
+				name = "";
+				frequency = "";
+				videoPID = "";
+				subChannel = "";
+				serviceID = "";
+
+				// get name excludes ";(null)"
+				int i = 0;
+				while (currentLine.charAt(i) != ';')
+					i++;
+
+				// get name move to frequency
+				name = currentLine.substring(0, i);
+				i += 8;
+				currentLine = currentLine.substring(i);
+				i = 0;
+
+				// get freqency adds 3 zeros
+				while (currentLine.charAt(i) != ':')
+					i++;
+
+				// get frequency move to videoPID
+				frequency = currentLine.substring(0, i) + "000";
+				i += 9;
+				currentLine = currentLine.substring(i);
+				i = 0;
+
+				// get videoPID
+				while (currentLine.charAt(i) != ':')
+					i++;
+
+				// ignore non atsc
+				if (!currentLine.substring(0, i).contains("="))
+					videoPID = currentLine.substring(0, i);
+
+				// move to end
+				i = currentLine.length() - 1;
+
+				// Find service id
+				while (currentLine.charAt(i) != ':')
+					i--;
+
+				// Get service id
+				serviceID = currentLine.substring(i + 1, currentLine.length());
+
+				i -= 6;
+				currentLine = currentLine.substring(0, i);
+				i = currentLine.length() - 1;
+
+				while (currentLine.charAt(i) != ':')
+					i--;
+
+				// Hope
+				subChannel = currentLine.substring(i + 1, currentLine.length());
+
+				// Write
+				if (!videoPID.equals(""))
+					bw.write(name + ":" + frequency + ":" + type + ":" + videoPID + ":" + serviceID + ":" + subChannel
+							+ "\n");
+			}
+
+			br.close();
+			bw.close();
+
+		} catch (FileNotFoundException e) {
+			System.out.println(input.toString() + " not found");
+		} catch (IOException e) {
+			System.out.println("File reading exception");
+			e.printStackTrace();
+		}
+	}
+}
